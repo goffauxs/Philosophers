1fa44eafff85855e449640cade7a635dbdd3dc87
Simon Goffaux <sgoffaux@fu-r8-p8.s19.be>
2021-07-13 12:08:04 +0200

    WIP header

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgoffaux <sgoffaux@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/13 12:06:45 by sgoffaux          #+#    #+#             */
+/*   Updated: 2021/07/13 12:07:54 by sgoffaux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+# include "libft.h"
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+#endif
