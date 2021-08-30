633fed210703afdeb43574e9f073f76756ed013a
Simon Goffaux <sgoffaux@fu-r9-p8.s19.be>
2021-08-30 12:00:39 +0200

    WIP

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: sgoffaux <sgoffaux@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 12:06:45 by sgoffaux          #+#    #+#             */
-/*   Updated: 2021/08/25 14:13:13 by sgoffaux         ###   ########.fr       */
+/*   Updated: 2021/08/30 11:59:52 by sgoffaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,29 @@
 # define ERR_USAGE "Usage: ./philo <# philosophers> <time to die> <time to eat>\
  <time to sleep> [# times each philosopher must eat]\n"
 
+struct s_env;
+
 typedef struct s_philo
 {
 	int				is_eating;
 	int				pos;
 	int				lfork;
 	int				rfork;
-	pthread_mutex_t	mutex;
-	int				time_since_last_ate;
+	unsigned long	last_ate;
+	unsigned long	limit;
+	struct s_env	*env;
+	pthread_t		thread_id;
 }				t_philo;
 
 typedef struct s_env
 {
-	int	count;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	eat_count_min;
-	t_philo	*philos;
+	int				count;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				eat_count_max;
+	unsigned long	start_time;
+	t_philo			*philos;
 	pthread_mutex_t	*forks;
 }				t_env;
 
--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,11 +6,52 @@
 /*   By: sgoffaux <sgoffaux@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 13:17:49 by sgoffaux          #+#    #+#             */
-/*   Updated: 2021/08/25 12:12:43 by sgoffaux         ###   ########.fr       */
+/*   Updated: 2021/08/30 11:59:04 by sgoffaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+static unsigned long	get_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * (unsigned long)1000) + (time.tv_usec / 1000));
+}
+
+static void	*routine(void *params)
+{
+	
+}
+
+static int	start_threads(t_env *env)
+{
+	int	i;
+
+	i = 0;
+	while (i < env->count)
+	{
+		if (pthread_create(&(env->philos[i].thread_id),
+			NULL, routine, &(env->philos[i])))
+			return (1);
+		env->philos[i].last_ate = get_time();
+		env->philos[i].limit = env->philos[i].last_ate + env->time_to_die;
+	}
+}
+
+static int	ft_init_mutex(t_env *env)
+{
+	int	i;
+
+	i = 0;
+	while (i < env->count)
+	{
+		if (pthread_mutex_init(&(env->forks[i]), NULL))
+			return (1);
+	}
+	return (0);
+}
 
 static void ft_init_philo(t_env *env)
 {
@@ -23,7 +64,7 @@ static void ft_init_philo(t_env *env)
 		env->philos[i].pos = i + 1;
 		env->philos[i].lfork = i;
 		env->philos[i].rfork = (i + 1) % env->count;
-		pthread_mutex_init(&env->philos[i].mutex, NULL);
+		env->philos[i].env = env;
 		i++;
 	}
 }
@@ -35,16 +76,24 @@ static int	ft_init(t_env *env, int argc, char *argv[])
 	env->time_to_eat = ft_atoi(argv[3]);
 	env->time_to_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
-		env->eat_count_min = ft_atoi(argv[5]);
+	{
+		env->eat_count_max = ft_atoi(argv[5]);
+		if (env->eat_count_max < 0)
+			return (1);
+	}
 	else
-		env->eat_count_min = 0;
+		env->eat_count_max = 0;
 	if (env->count < 2 || env->count > 200 || env->time_to_die < 60
 		|| env->time_to_eat < 60 || env->time_to_sleep < 60
-		|| env->eat_count_min < 0)
+		|| env->eat_count_max < 0)
 		return (0);
-	env->philos = (t_philo *)malloc(sizeof(t_philo) * env->count);
+	env->philos = malloc(sizeof(t_philo) * env->count);
 	if (!env->philos)
 		return (0);
+	env->forks = malloc(sizeof(pthread_mutex_t) * env->count);
+	if (!env->forks)
+		return (0);
+	ft_init_mutex(env);
 	ft_init_philo(env);
 	return (1);
 }
@@ -57,9 +106,6 @@ int	main(int argc, char *argv[])
 		return (ft_return_error(ERR_USAGE));
 	if (!ft_init(&env, argc, argv))
 		return (ft_return_error("init error.\n"));
-	printf("ttd: %d | tte: %d | tts: %d\n", env.time_to_die, env.time_to_eat, env.time_to_sleep);
-	for (int i = 0; i < env.count; i++)
-	{
-		printf("philo #%d: lfork %d | rfork %d\n", i, env.philos[i].lfork, env.philos[i].rfork);
-	}
+	if (start_threads(&env))
+		return (ft_return_error("threads error.\n"));
 }
